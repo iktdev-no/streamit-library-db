0736f24beacfc19953969bd5ad54df9886bc94b1
Brage <bskjon@outlook.com>
2023-07-26 16:41:31 +0200

    Updated to use a default

--- src/main/kotlin/no/iktdev/streamit/library/db/tables/CatalogTable.kt
+++ src/main/kotlin/no/iktdev/streamit/library/db/tables/CatalogTable.kt
@@ -4,9 +4,12 @@ import no.iktdev.streamit.library.db.tables.progress.clientDefault
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Column
 import org.jetbrains.exposed.sql.insert
+import org.jetbrains.exposed.sql.javatime.CurrentDateTime
+import org.jetbrains.exposed.sql.javatime.datetime
 import org.jetbrains.exposed.sql.javatime.timestamp
 import org.jetbrains.exposed.sql.transactions.transaction
 import java.time.Instant
+import java.time.LocalDateTime
 
 object catalog : IntIdTable() {
     val title: Column<String> = varchar("title", 250)
@@ -15,7 +18,8 @@ object catalog : IntIdTable() {
     var collection: Column<String> = varchar("collection", 100)
     var iid: Column<Int?> = integer("iid").nullable()
     var genres: Column<String?> = varchar("genres", 24).nullable()
-    val added: Column<Instant> = timestamp("added").clientDefault { Instant.now() }
+    val added: Column<LocalDateTime> = datetime("added").defaultExpression(CurrentDateTime)
+
 
     init {
         uniqueIndex(title, type)
--- src/main/kotlin/no/iktdev/streamit/library/db/tables/ProgressTable.kt
+++ src/main/kotlin/no/iktdev/streamit/library/db/tables/ProgressTable.kt
@@ -3,8 +3,11 @@ package no.iktdev.streamit.library.db.tables
 import no.iktdev.streamit.library.db.tables.catalog.default
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.Column
+import org.jetbrains.exposed.sql.javatime.CurrentDateTime
+import org.jetbrains.exposed.sql.javatime.datetime
 import org.jetbrains.exposed.sql.javatime.timestamp
 import java.time.Instant
+import java.time.LocalDateTime
 
 object progress : IntIdTable() {
     val guid: Column<String> = varchar("guid", 50)
@@ -16,7 +19,7 @@ object progress : IntIdTable() {
     val video: Column<String> = varchar("video", 100)
     val progress: Column<Int> = integer("progress")
     val duration: Column<Int> = integer("duration")
-    val played: Column<Instant?> = timestamp("played").clientDefault { Instant.now() }.nullable()
+    val played: Column<LocalDateTime?> = datetime("played").defaultExpression(CurrentDateTime).nullable()
 
     init {
 
