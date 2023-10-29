c515f705264311a1a0da8011b7768ad3fa551c8a
Brage <brage.skjonborg@hotmail.no>
2023-10-29 23:25:06 +0100

    Using manual upsert

--- src/main/kotlin/no/iktdev/streamit/library/db/query/ResumeOrNextQuery.kt
+++ src/main/kotlin/no/iktdev/streamit/library/db/query/ResumeOrNextQuery.kt
@@ -4,7 +4,9 @@ import no.iktdev.streamit.library.db.tables.catalog
 import no.iktdev.streamit.library.db.tables.executeWithStatus
 import no.iktdev.streamit.library.db.tables.insertWithSuccess
 import no.iktdev.streamit.library.db.tables.resumeOrNext
-import org.jetbrains.exposed.sql.upsert
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import org.jetbrains.exposed.sql.javatime.CurrentDateTime
 
 class ResumeOrNextQuery(
     val userId: String,
@@ -14,20 +16,42 @@ class ResumeOrNextQuery(
     val episode: Int? = null,
     val season: Int? = null,
     val video: String
-): CommonQueryFuncions {
+) : CommonQueryFuncions {
 
-    override fun insertAndGetStatus(): Boolean {
+    fun upsertAndGetStatus(): Boolean {
         return executeWithStatus {
-            resumeOrNext.upsert(resumeOrNext.collection, resumeOrNext.type, resumeOrNext.userId) {
-                it[userId] = userId
-                it[type] = type
-                if (this@ResumeOrNextQuery.ignore != null) {
-                    it[ignore] = this@ResumeOrNextQuery.ignore
+            val isPresent = resumeOrNext.select(
+                resumeOrNext.collection.eq(collection)
+                    .and(resumeOrNext.type.eq(type))
+                    .and(resumeOrNext.userId.eq(userId))
+            ).singleOrNull() != null
+
+            if (!isPresent) {
+                resumeOrNext.insert {
+                    it[userId] = this@ResumeOrNextQuery.userId
+                    it[type] = this@ResumeOrNextQuery.type
+                    if (this@ResumeOrNextQuery.ignore != null) {
+                        it[ignore] = this@ResumeOrNextQuery.ignore
+                    }
+                    it[collection] = this@ResumeOrNextQuery.collection
+                    it[episode] = this@ResumeOrNextQuery.episode
+                    it[season] = this@ResumeOrNextQuery.season
+                    it[video] = this@ResumeOrNextQuery.video
                 }
-                it[collection] = collection
-                it[episode] = episode
-                it[season] = season
-                it[video] = video
+            } else {
+                resumeOrNext.update({
+                    resumeOrNext.collection.eq(collection)
+                        .and(resumeOrNext.type.eq(type))
+                        .and(resumeOrNext.userId.eq(userId))
+                }) {
+                    if (this@ResumeOrNextQuery.ignore != null) {
+                        it[ignore] = this@ResumeOrNextQuery.ignore
+                    }
+                    it[episode] = episode
+                    it[season] = season
+                    it[video] = video
+                    it[updated] = CurrentDateTime
+                }
             }
         }
     }
