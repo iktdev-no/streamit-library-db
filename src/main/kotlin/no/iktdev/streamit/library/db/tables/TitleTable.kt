20bf9bceedcb5546bdee29ede1bcf19281382aa4
bskjon <bskjon@outlook.com>
2024-04-14 22:48:30 +0200

    Adding new table

--- src/main/kotlin/no/iktdev/streamit/library/db/tables/TitleTable.kt
+++ src/main/kotlin/no/iktdev/streamit/library/db/tables/TitleTable.kt
@@ -6,6 +6,7 @@ import org.jetbrains.exposed.sql.Column
 object titles: IntIdTable() {
     val collection: Column<String> = varchar("collection", 250)
     val title: Column<String> = varchar("title", 250)
+    val type: Column<Int> = integer("type").default(3)
 
     init {
         uniqueIndex(collection, title)
