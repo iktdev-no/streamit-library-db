df70ba06ce68d87f7357a3cd8ed506b70d198beb
bskjon <bskjon@outlook.com>
2024-05-09 00:16:24 +0200

    Updated

--- src/main/kotlin/no/iktdev/streamit/library/db/tables/registeredDevices.kt
+++ src/main/kotlin/no/iktdev/streamit/library/db/tables/registeredDevices.kt
@@ -9,7 +9,8 @@ import java.time.LocalDateTime
 object registeredDevices : IntIdTable() {
     val deviceId: Column<String> = varchar("deviceId", 256)
     val applicationPackageName: Column<String> = varchar("applicationPackageName", 32)
-    val androidVersion: Column<String> = varchar("androidVersion", 28)
+    val osVersion: Column<String> = varchar("osVersion", 28)
+    val osPlatform: Column<String> = varchar("osPlatform", 28)
     val registered: Column<LocalDateTime> = datetime("registered").defaultExpression(CurrentDateTime)
     val lastSeen: Column<LocalDateTime> = datetime("lastSeen").defaultExpression(CurrentDateTime)
 }
