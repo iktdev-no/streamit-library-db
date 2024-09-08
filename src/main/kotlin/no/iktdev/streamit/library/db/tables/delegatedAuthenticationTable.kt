78b4b9dafa8ac621c73a8f1bd40cbe5f7296233c
bskjon <bskjon@outlook.com>
2024-09-08 16:28:26 +0200

    Fix for time

--- src/main/kotlin/no/iktdev/streamit/library/db/tables/delegatedAuthenticationTable.kt
+++ src/main/kotlin/no/iktdev/streamit/library/db/tables/delegatedAuthenticationTable.kt
@@ -12,8 +12,8 @@ object delegatedAuthenticationTable: IntIdTable() {
     val pin: Column<String> = varchar("pin", 8)
     val requesterId: Column<String> = char("requesterId", 64)
     val deviceInfo: Column<String> = varchar("deviceInfo", 256)
-    val created: Column<LocalDateTime> = datetime("created").defaultExpression(CurrentDateTime)
-    val expires: Column<LocalDateTime> = datetime("expires").clientDefault { LocalDateTime.now().plusMinutes(15) }
+    val created: Column<LocalDateTime> = datetime("created").clientDefault { LocalDateTime.now() }
+    val expires: Column<LocalDateTime> = datetime("expires").clientDefault {  LocalDateTime.now().plusMinutes(15) }
     val permitted: Column<Boolean> = bool("permitted").default(false)
     val consumed: Column<Boolean> = bool("consumed").default(false)
     val method = enumerationByName("method", 3, AuthMethod::class) // Brukt metode (PIN eller QR)
